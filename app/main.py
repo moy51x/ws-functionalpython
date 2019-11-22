7484d3e639a5c97aa6cb1f417c7e1b0a3f286550
MoySix <mmorales@nearsoft.com>
2019-11-22 12:56:10 -0600

    Init python file was removed.

---
+++ app/main.py
@@ -0,0 +1,12 @@
+# Code to start the app
+from .menu import greet
+
+def main():
+    print("Hello, World! " + __name__ )
+    
+def another():
+    print("Hello... " + __name__)
+
+
+if __name__== "__main__" :
+    main()
---
+++ app/menu.py
@@ -0,0 +1,8 @@
+# Code to show the main menu
+
+
+def greet():
+    print("Hi! " + __name__)
+
+if __name__=="__main__":
+    greet()
