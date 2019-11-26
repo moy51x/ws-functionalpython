2e80cb3a79a7f4025302de9c92a87c3ed055a312
MoySix <mmorales@nearsoft.com>
2019-11-25 21:00:40 -0600

    Generador Listo!

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@
 from .menu import creaMenu
 from .constants import METHOD_TOP_ARTISTS, METHOD_TOP_TRACKS
 
+
 def main():
+    datos = {}
     menu, funciones = creaMenu()
 
     while True:
@@ -14,7 +16,7 @@ def main():
             if opcion > len(menu):
                 print("Opcion invalida!")
             elif int(opcion)>=0:
-                ejecutaOpcion(menu, funciones, opcion)
+                datos = ejecutaOpcion(menu, funciones, datos, opcion)
 
 
 def muestraMenu(menu):
@@ -23,16 +25,33 @@ def muestraMenu(menu):
     return input("Selecciona una opcion: ")
 
 
-def ejecutaOpcion(menu, funciones, opcion):
+def ejecutaOpcion(menu, funciones, datos, opcion):
     opcion_seleccionada = menu[opcion]
     funcion = funciones[opcion_seleccionada]
     if opcion_seleccionada == menu[0]:
-        funcion(METHOD_TOP_ARTISTS) #Descarga Top Artistas
+        datos = funcion(METHOD_TOP_ARTISTS) #Descarga Top Artistas
+        #print(datos)
     elif opcion_seleccionada == menu[1]:
-        funcion(METHOD_TOP_TRACKS) #Descarga Top Tracks
+        datos = funcion(METHOD_TOP_TRACKS) #Descarga Top Tracks
+        #print(datos)
+    elif opcion_seleccionada == menu[4]:
+        muestraLosDatos(datos)
     else:
         funcion()
+    
+    return datos
         
+
+def muestraLosDatos(datos):
+    for cadena in dameDatoFormateado(datos):
+        print(cadena)
+
+
+def dameDatoFormateado(datos):
+    base_str = "Nombre: {0}, Radioescuchas: {1}, URL: {2}"
+    for dato in datos:
+        yield base_str.format(dato["name"], dato["listeners"], dato["url"])
+
 
 if __name__== "__main__" :
     main()
